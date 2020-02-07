a5919fba67c4de55997cbf467b976b8ee705752f
Joe Vargas <joeavargas@outlook.com>
2020-02-07 02:26:16 -0600

    Database setup and saving data to Firestore successful.

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        FirebaseApp.configure()
+        configureFirestore()
         
         return true
     }
@@ -33,6 +33,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    //MARK: - Configure Firestore
+    func configureFirestore(){
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
     }
 
 
